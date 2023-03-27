413d0317a4c9408edd7e65ec94fb18e8de96f967
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-28 00:13:12 +0200

    Update Education.js

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -10,9 +10,9 @@ import { IoMdSchool } from "react-icons/io";
 function Education() {
   return (
     <VerticalTimeline>
-    </br>
-    </br>
-    </br>
+    <br />
+    <br />
+    <br />
   <VerticalTimelineElement"
     className="vertical-timeline-element--work"
     contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
