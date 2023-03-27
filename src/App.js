4e00ba8bc6dcc1a5fb2da62d1cfd3c285114e312
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-27 06:37:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
+          <Route path="/project" element={<ComingSoon />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/education" element={<ComingSoon />} />
