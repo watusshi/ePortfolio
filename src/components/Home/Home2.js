71fda1288a6803c5dedb8d31c34391c2de26cf3c
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-27 07:29:40 +0200

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -43,7 +43,7 @@ function Home2() {
               <i>
                 <b className="purple">
                   {" "}
-                  JAVA
+                  JAVA.
                 </b>
               </i>
             </p>
