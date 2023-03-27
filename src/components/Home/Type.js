bb57c0abc425d8bc23aae1a92bdfff0d0f4d0c64
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-27 06:12:00 +0200

    Add files via upload

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
+          "Computer Science Student at ENSEEIHT",
           "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
         ],
         autoStart: true,
         loop: true,
