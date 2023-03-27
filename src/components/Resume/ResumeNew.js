896c36a45f78667da883b05690e9077ed40bcf01
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-27 06:06:31 +0200

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Abdelmouhaimen/ePortfolio/main/src/Assets/Abdelmouhaimen_Sarhane.pdf";
+  "https://raw.githubusercontent.com/Abdelmouhaimen/ePortfolio/main/src/Assets/Abdelmouhaimen_Sarhanee.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
