04e88b363d583d64e7d2040f37f7f90af00f7020
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-27 06:43:56 +0200

    Update ComingSoon.js

--- src/components/ComingSoon/ComingSoon.js
+++ src/components/ComingSoon/ComingSoon.js
@@ -3,9 +3,32 @@ import React from "react";
 
 function ComingSoon() {
   return (
-    <section>
-      Coming Soon
-    </section>
+    <Container fluid className="about-section">
+      <Particle />
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          >
+            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              Know Who <strong className="purple">Coming Soon ...</strong>
+            </h1>
+          </Col>
+          <Col
+            md={5}
+            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            className="about-img"
+          >
+            <img src={laptopImg} alt="about" className="img-fluid" />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   );
 }
 
