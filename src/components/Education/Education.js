d06f3ddc12173a6b19fa60616ca4698fda3ef871
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-28 00:04:34 +0200

    Update Education.js

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -40,7 +40,7 @@ function Education() {
     className="vertical-timeline-element--work"
     date="2008 - 2010"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    icon={<IoMdSchooln />}
+    icon={<IoMdSchool />}
   >
     <h3 className="vertical-timeline-element-title">Web Designer</h3>
     <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
@@ -88,7 +88,7 @@ function Education() {
     className="vertical-timeline-element--education"
     date="2002 - 2006"
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    icon={<SchoolIcon />}
+    icon={<IoMdSchool />}
   >
     <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
     <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
@@ -98,7 +98,7 @@ function Education() {
   </VerticalTimelineElement>
   <VerticalTimelineElement
     iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-    icon={<SchoolIcon />}
+    icon={<IoMdSchool />}
   />
 </VerticalTimeline>
   );
