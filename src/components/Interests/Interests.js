9561a18019a564fc21dbfbd6f6014b6171dd3061
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-28 01:28:54 +0200

    Update Interests.js

--- src/components/Interests/Interests.js
+++ src/components/Interests/Interests.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import InterestCard from "./InterestCards";
 import Particle from "../Particle";
-import postgresql from "../../Assets/Projects/PostegreSQL.png";
-import tipe from "../../Assets/Projects/tipe.png";
-import myjobglasses from "../../Assets/Projects/myjobglasses.png";
+import gaming from "../../Assets/Interests/gaming.png";
+import sports from "../../Assets/Interests/sports.png";
+import coding from "../../Assets/Interests/coding.png";
+import science from "../../Assets/Interests/science.png";
+
 
 function Interests() {
   return (
@@ -12,41 +14,48 @@ function Interests() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          What Else <strong className="purple"> I do </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Being able to study at school, exercise my body and entertain myself with several activities is an important key to productivity and creativity, that is why I make sure to stay productive and enjoy my time as an engineering student
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <InterestCard
-              imgPath={postgresql}
+              imgPath={gaming}
               isBlog={false}
-              title="A PostgreSQL Administrator"
-              description="PostgreSQL Browser is a web-based PostgreSQL database admin tool written in python using Flask, HTML, CSS, JavaScript. It is a tool that will help you view your database from Heroku and edit them directly with a more friendly interface. This project was part of my final project to validate my cs50x certification."
-              ghLink="https://github.com/Abdelmouhaimen/browser"
-              demoLink="https://spark.adobe.com/video/rv3QwkwFtNMfx"
+              title="Video Games"
+              description="I am passionate about competitive video games, especially FPS games, which requires strategy and good reflexes. I have been an in game leader for competitive teams in order to deploy strategies to win our games."
             />
           </Col>
 
           <Col md={4} className="project-card">
             <InterestCard
-              imgPath={tipe}
+              imgPath={sport}
               isBlog={false}
-              title="TIPE : Heart Rate Monitor"
-              description="Supervised personal initiative project or TIPE is a common test to most entrance examinations to the Grandes Ecoles of Science. During my preparatory classes, I made a heart rate monitor as a project for my TIPE. The device measured heart rate using a technique called Photoplethysmography. I had to make the sensor using IR LED also photodiode, also I had make the electronic circuit to ensure the output contains only the alternative signal from heart pulse. Finally I did the post-processing using Arduino kit to visualise the heart rate."
+              title="Sports"
+              description="Since my childhood, I have been passionate about sports especially football."
             />
           </Col>
 
           <Col md={4} className="project-card">
             <InterestCard
-              imgPath={myjobglasses}
+              imgPath={coding}
               isBlog={false}
-              title="MyJobGlasses Certificate"
-              description="The certificate is a document designed by My Job Glasses to promote the approach of young people in their orientation. It attests to their ability to contact and meet mentors on the My Job Glasses mentoring tool to: find out more about a job or training | develop their professional network | expand their knowledge of an industry or company | get a professional opportunity such as an internship or work-study"           
+              title="Coding"
+              description="There are so many things to love about coding, both from a technical and philosophical perspective. It can sharpen your mind, rewire your brain, provide people with great software... the options and impact are nearly endless. Programming can really empower to change the world."           
             />
           </Col>
-
+          
+          <Col md={4} className="project-card">
+            <InterestCard
+              imgPath={science}
+              isBlog={false}
+              title="Science"
+              description="I am interested in all forms of science, but mathematics are special, that's why after high school, I chose to pursuit a two-year intensive program of preparatory classes to the competitive entrance exams to the top engineering schools."           
+            />
+          </Col>
+    
         </Row>
       </Container>
     </Container>
