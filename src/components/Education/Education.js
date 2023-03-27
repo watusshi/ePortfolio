7db7c63c55a2ba64a0f72a1422f1fb835dc8eff4
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-27 23:31:03 +0200

    Update Education.js

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,36 +1,13 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
-import Particle from "../Particle";
 
 
-function ComingSoon() {
+
+function Education() {
   return (
-    <div className={styles.education}>
-      <motion.div
-        initial='init'
-        animate='anim'
-        exit='last'
-        variants={pageVariants}
-        transition={pageTransition}
-      >
-        <div className={styles.wrapper}>
-          <h3 className={styles.eduOpen}>{eduOpen}</h3>
-          <div className={styles.center_line}></div>
-          {ExperienceData.map((item, index) => (
-            <EducationCard
-              key={item.title}
-              title={item.title}
-              date={item.date}
-              details={item.details}
-              isLeft={index % 2 === 0 ? true : false}
-            />
-          ))}
-          <h3 className={styles.eduClose}>{eduClose}</h3>
-        </div>
-      </motion.div>
+    <div>
+      Nothing here
     </div>
   );
 }
 
-export default ComingSoon;
+export default Education;
