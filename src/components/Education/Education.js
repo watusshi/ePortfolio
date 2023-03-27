bdfd797c740e1e399631c8662e29222b92faf449
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-27 23:24:22 +0200

    Update Education.js

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
-import Type from "./Type";
+
 
 function ComingSoon() {
   return (
