99dea0629b53a1cd0288a1786bcadb1fb8972c27
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-29 06:40:34 +0200

    Update and rename InternationalCards.js to Cards.js

--- src/components/International/Cards.js
+++ src/components/International/Cards.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
-function InternationalCards(props) {
+function Cards(props) {
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
@@ -39,4 +39,4 @@ function InternationalCards(props) {
     </Card>
   );
 }
-export default InternationalCards;
+export default Cards;
