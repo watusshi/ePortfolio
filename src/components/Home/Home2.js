9ce0056e035251c28cf6109ec5e717469ea46eb1
Abdelmouhaimen <68982208+Abdelmouhaimen@users.noreply.github.com>
2023-03-31 02:21:01 +0200

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,7 +24,7 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> JAVA, C, Javascript and Python. </b>
               </i>
               <br />
               <br />
