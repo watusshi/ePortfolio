d7131b589856a25edb31ad428fd1c591bede0dd1
Thai Lai <khanhthailai@gmail.com>
2024-04-23 15:16:22 -0500

    Changed username to watusshi

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,7 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="Abdelmouhaimen"
+        username="watusshi"
         blockSize={15}
         blockMargin={5}
         color="#3161c2"
