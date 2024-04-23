c65a657fc6272acf6b198d1a9d01cfd92bee3464
Thai Lai <khanhthailai@gmail.com>
2024-04-23 15:12:46 -0500

    Updated personal biography

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,11 +9,11 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Thai Lai </span>
-            <br />Greetings! My name is Thai Lai, an Information Technology student at the University of Texas at Arlington, due to graduate May 2025. My primary interest lies within technologies and coding. I am enthusiastic about advancing my pursuits in this field, both academically and professionally. I have proven the ability to leverage full-stack Software Engineer as well as experienced in design and building any graphics software from ground up.
+            <br />Greetings! My name is Thai Lai, an Information Technology student at the University of Texas at Arlington, due to graduate May 2025. 
 
 
             <br />
-            <br />This portfolio seeks to best convey my character, my professional trajectory, and my future goals. You can find information about my experiences, initiatives, and skills, both professionally and personally. Please get in touch with me if you want additional information about my experience or the projects I worked on.
+            <br />My primary interest lies within technologies and coding. I am enthusiastic about advancing my pursuits in this field, both academically and professionally. I have proven the ability to leverage full-stack Software Engineer as well as experienced in design and building any graphics software from ground up.
             <br />
             <br />
             <br />Apart from coding, some other activities that I love to do!
