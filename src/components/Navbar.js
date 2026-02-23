df3957ba689de6efd52657c27975563b747d5175
Thai Lai <khanhthailai@gmail.com>
2026-02-23 15:08:49 -0600

    Updated the Nav Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,12 +15,7 @@ import {
 
 import { CgFileDocument } from "react-icons/cg";
 import { FaEnvira } from "react-icons/fa";
-import { GiWorld } from "react-icons/gi";
-import { IoMdSchool } from "react-icons/io";
 import { MdOutlineSportsEsports } from "react-icons/md";
-
-
-
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -95,26 +90,6 @@ function NavBar() {
                 onClick={() => updateExpanded(false)}
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/education"
-                onClick={() => updateExpanded(false)}
-              >
-                <IoMdSchool style={{ marginBottom: "2px" }} /> Career
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/international"
-                onClick={() => updateExpanded(false)}
-              >
-                <GiWorld style={{ marginBottom: "2px" }} /> Mobility
               </Nav.Link>
             </Nav.Item>
 
